39c62e4156f525f4482dc1e9caec0247b3454ce9
Eric Chiu <c3366301@gmail.com>
2018-12-08 22:05:39 +0800

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_12_07_224310) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
