6c630baf3956f0b97a2a52195271e546a46aefbc
Eric Chiu <c3366301@gmail.com>
2018-12-10 18:24:30 +0800

    rename spec/views

--- spec/views/admin/product/index_spec.rb
+++ spec/views/admin/product/index_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "admin/products/index", type: :view do
   it "can renders with login" do
     @users = User.all
+    @products = Product.all
     current_user = User.find_by(email: "admin01@gmail.com")
     allow(view).to receive(:current_user).and_return(current_user)
     render
